befe3c624f965ecb286abc44aa7d8e2965a485d4
Willian Simões <williansp@protonmail.com>
2025-08-16 19:36:03 -0300

    Initial Spring Boot project setup

---
+++ rioadentro-demo/src/main/java/com/rioadentro/rioadentro_demo/RioadentroDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rioadentro.rioadentro_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RioadentroDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RioadentroDemoApplication.class, args);
+	}
+
+}
